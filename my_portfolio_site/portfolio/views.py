a39517e06ad38addabb8b73e2553fc3beb322e05
SGGM <gleb_somov@mail.ru>
2022-12-14 18:59:09 +0300

    added home, post, posts, profile, navbar and main templates for now.

--- my_portfolio_site/portfolio/views.py
+++ my_portfolio_site/portfolio/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+def posts(request):
+    return render(request, 'portfolio/posts.html')
+
+def post(request):
+    return render(request, 'portfolio/post.html')
+
+def profile(request):
+    return render(request, 'portfolio/profile.html')
